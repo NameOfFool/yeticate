82810b1c2d187be1a3f870dfe187faf240030026
NameOfFool <105356438+LetMeInGodDamit@users.noreply.github.com>
2022-05-13 11:54:47 +0300

    13.05.22 fixed

--- db.sql
+++ db.sql
@@ -24,14 +24,11 @@ create table announcement(
 create table Bet(
                   ID_bet int PRIMARY KEY AUTO_INCREMENT,
                   ID_user int not null,
-                  ID_announcement int not null,
                   Date_of_accommodation datetime not null,
                   Sum int not null
 );
 create table User(
                    ID_user int primary key AUTO_INCREMENT,
-                   ID_announcement int not null,
-                   ID_bet int not null,
                    Date_of_registration datetime not null,
                    Email varchar(50) not null unique,
                    User_name varchar(50) not null unique,
@@ -44,8 +41,4 @@ alter table announcement
     add foreign key (ID_creator) references User(ID_user) ON DELETE CASCADE ON UPDATE CASCADE,
     add foreign key (ID_winner) references  User(ID_user) ON DELETE CASCADE ON UPDATE CASCADE;
 alter table bet
-  add foreign key (ID_user) references User(ID_user) ON DELETE CASCADE ON UPDATE CASCADE,
-    add foreign key (ID_announcement) references announcement(ID_announcements) ON DELETE CASCADE ON UPDATE CASCADE;
-alter table user
-  add foreign key (ID_announcement) references announcement(ID_announcements) ON DELETE CASCADE ON UPDATE CASCADE,
-    add foreign key (ID_bet) references bet(ID_bet)ON DELETE CASCADE ON UPDATE CASCADE;
+  add foreign key (ID_user) references User(ID_user) ON DELETE CASCADE ON UPDATE CASCADE;
